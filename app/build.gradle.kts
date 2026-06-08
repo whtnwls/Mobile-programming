1edf07632b95638e608c710c00db16b4dd6b2078
whtnwls <sujin200507@gmail.com>
2026-06-08 15:49:27 +0900

    권한 화면 및 이미지 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.cardview:cardview:1.0.0")
 }
