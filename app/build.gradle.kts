79107394447857e34518938c66faaa04389559d3
whtnwls <sujin200507@gmail.com>
2026-06-08 18:02:48 +0900

    피드백 화면 및 기록 화면

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
