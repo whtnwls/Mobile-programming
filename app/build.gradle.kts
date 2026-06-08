42ee70026bfabbbfae96b8a85b297d7720f4e4e6
whtnwls <sujin200507@gmail.com>
2026-06-08 16:09:08 +0900

    메인화면

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
